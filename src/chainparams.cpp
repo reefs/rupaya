6aec44643fc81134a3ddee9562341a5e30d2c297
Rupaya <6757341+rupaya-project@users.noreply.github.com>
2018-04-12 17:35:21 +0500

    change testnet magic

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -231,10 +231,10 @@ public:
     {
         networkID = CBaseChainParams::TESTNET;
         strNetworkID = "test";
-        pchMessageStart[0] = 0x43;
-        pchMessageStart[1] = 0x76;
-        pchMessageStart[2] = 0x65;
-        pchMessageStart[3] = 0xba;
+        pchMessageStart[0] = 0x7c;
+        pchMessageStart[1] = 0xba;
+        pchMessageStart[2] = 0x1b;
+        pchMessageStart[3] = 0x54;
         vAlertPubKey = ParseHex("042292b1f401860eea99e1a8a103effbd7e1c013a59a1a3a0c91c9d1997a0bc6f338567278c11344802838c107055bf7c1641eaed61e879245c255a4f5be5746fc");
         nDefaultPort = 51434;
         nEnforceBlockUpgradeMajority = 51;
